5069ed19a6762665c647f4dc42b11b0532479911
Patrick M. Roskam <pat.roskam@gmail.com>
2018-04-27 07:18:13 -0700

    initial commit

---
+++ ImagesDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagesDemo
+//
+//  Created by Patrick Roskam on 4/27/18.
+//  Copyright © 2018 PatrickMRoskam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
